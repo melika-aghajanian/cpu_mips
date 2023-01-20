05bc01e2f506c627c32f06aaf3ad984c2bf034e1
Melika AghajanianSabagh <84022036+melika-aghajanian@users.noreply.github.com>
2023-01-20 00:19:44 -0800

    Add files via upload

---
+++ phase 2/verilog/DATA MEMORY/Data_Mem.v
@@ -0,0 +1,39 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    18:19:14 01/18/2023 
+// Design Name: 
+// Module Name:    Data_Mem 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module Data_Mem(input[15:0] address, input[15:0] write_data,
+					 input memw, input memr, input clk, output[15:0] read_data);
+					 
+		reg[15:0] memory [0:65535];
+	/* for testing
+	initial
+	begin
+		memory[0] = 32'b1010111;//87
+		memory[1] = 32'b1010011;//83
+		memory[2] = 32'b100110;//38
+		memory[3] = 32'b11100;//28
+	end
+	//*/
+	always @(posedge clk) begin
+		if (memw)
+			memory[address] <= write_data;
+	end
+	assign read_data = (memr) ? memory[address] : 0;
+endmodule
---
+++ phase 2/verilog/DATA MEMORY/Data_Mem_test.v
@@ -0,0 +1,78 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   18:52:56 01/18/2023
+// Design Name:   Data_Mem
+// Module Name:   E:/CA-prj/Data_Mem_test.v
+// Project Name:  ALU
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: Data_Mem
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module Data_Mem_test;
+
+	// Inputs
+	reg [15:0] address;
+	reg [15:0] write_data;
+	reg memw;
+	reg memr;
+	reg clk;
+
+	// Outputs
+	wire [15:0] read_data;
+
+	// Instantiate the Unit Under Test (UUT)
+	Data_Mem uut (
+		.address(address), 
+		.write_data(write_data), 
+		.memw(memw), 
+		.memr(memr), 
+		.clk(clk), 
+		.read_data(read_data)
+	);
+
+	initial begin
+		// Initialize Inputs
+		address = 0;
+		write_data = 0;
+		memw = 0;
+		memr = 0;
+		clk = 0;
+
+		// Wait 100 ns for global reset to finish
+		#100;
+        
+		// Add stimulus here
+		address = 0;
+		memr = 1;
+		#100;
+		address = 4;
+		memw = 1;
+		memr = 0;
+		write_data = 25;
+		#100;
+		memw = 0;
+		memr = 1;
+		#100;
+		address = 1;
+		#100;
+		address = 2;
+		#100;
+		address = 3;
+		#100;
+	end
+	always #20 clk = ~clk;
+endmodule
